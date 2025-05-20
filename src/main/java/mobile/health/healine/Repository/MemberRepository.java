695e1bbce7c079c8f05ace09fbb7b615415bba3e
monikhyun <mih2001103@naver.com>
2025-05-20 13:50:11 +0900

    feat : MemberRepository 수정

--- src/main/java/mobile/health/healine/Repository/MemberRepository.java
+++ src/main/java/mobile/health/healine/Repository/MemberRepository.java
@@ -2,7 +2,9 @@ package mobile.health.healine.Repository;
 
 import mobile.health.healine.Entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Member findByUserId(String userId);
