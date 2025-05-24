4155589894548d0990367a2cf90e4c8844197d24
monikhyun <mih2001103@naver.com>
2025-05-25 03:46:09 +0900

    fix : ENUM(BodyPart) 수정

--- src/main/java/mobile/health/healine/Entity/BodyPart.java
+++ src/main/java/mobile/health/healine/Entity/BodyPart.java
@@ -1,9 +1,32 @@
 package mobile.health.healine.Entity;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum BodyPart {
-    BACK,       // 등
-    LOWER_BODY, // 하체
-    SHOULDERS,  // 어깨
-    CHEST,      // 가슴
-    ARMS        // 팔
+    BACK("등"),
+    LOWER_BODY("하체"),
+    SHOULDERS("어깨"),
+    CHEST("가슴"),
+    ARMS("팔");
+
+    private final String displayName;
+    public String getDisplayName() {
+        return displayName;
+    }
+    BodyPart(String displayName) {
+        this.displayName = displayName;
+    }
+    public static BodyPart fromDisplayName(String name){
+        for (BodyPart bp : values()){
+            if (bp.displayName.equals(name)) return bp;
+        }
+        throw new IllegalArgumentException("Unknown BodyPart: " + name);
+    }
+    @Override
+    public String toString() {
+        return displayName;
+    }
+
+    @JsonValue
+    public String toJson() { return displayName; }
 }
