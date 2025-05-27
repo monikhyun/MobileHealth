90ae00c958c4292fb41539c0ac2a8291a327163d
kimjintae <verbal0719@naver.com>
2025-05-28 02:28:30 +0900

    feat : 지연 주기

--- app/src/main/java/com/example/resister/DietActivity.java
+++ app/src/main/java/com/example/resister/DietActivity.java
@@ -8,18 +8,21 @@ import android.widget.Toast;
 import android.widget.LinearLayout;
 import android.view.View;
 import android.widget.TextView;
+import android.os.Handler;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONArray;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.anychart.AnyChart;
 import com.anychart.AnyChartView;
 import com.anychart.charts.Pie;
@@ -37,87 +40,82 @@ public class DietActivity extends AppCompatActivity {
 
     private String userId;
 
-
-
     // Fetch diet data from server and update chart and meal list
     private void fetchDietData(String today) {
         String url = "http://10.0.2.2:8080/api/diet/" + userId + "?date=" + today;
 
         JsonArrayRequest request = new JsonArrayRequest(
-            Request.Method.GET,
-            url,
-            null,
-            response -> {
-                try {
-                    // Prepare for summing nutrients and clearing previous views
-                    double totalCarb = 0;
-                    double totalProtein = 0;
-                    double totalFat = 0;
-                    BigDecimal totalkcal =BigDecimal.ZERO;
+                Request.Method.GET,
+                url,
+                null,
+                response -> {
+                    try {
+                        // Prepare for summing nutrients and clearing previous views
+                        double totalCarb = 0;
+                        double totalProtein = 0;
+                        double totalFat = 0;
+                        BigDecimal totalkcal = BigDecimal.ZERO;
 
-                    List<DataEntry> data = new ArrayList<>();
-                    LinearLayout mealContainer = findViewById(R.id.mealContainer);
-                    mealContainer.removeAllViews();
+                        List<DataEntry> data = new ArrayList<>();
+                        LinearLayout mealContainer = findViewById(R.id.mealContainer);
+                        mealContainer.removeAllViews();
 
-                    // Parse each meal entry
-                    for (int i = 0; i < response.length(); i++) {
-                        JSONObject meal = response.getJSONObject(i);
-                        String name = meal.getString("name");
-                        int carbMeal = meal.getInt("carb");
-                        int proteinMeal = meal.getInt("protein");
-                        int fatMeal = meal.getInt("fat");
-                        String mealtime = meal.getString("mealtime");
-                        BigDecimal kcalMeal = BigDecimal.valueOf(meal.getDouble("calories"));
+                        // Parse each meal entry
+                        for (int i = 0; i < response.length(); i++) {
+                            JSONObject meal = response.getJSONObject(i);
+                            String name = meal.getString("name");
+                            int carbMeal = meal.getInt("carb");
+                            int proteinMeal = meal.getInt("protein");
+                            int fatMeal = meal.getInt("fat");
+                            String mealtime = meal.getString("mealtime");
+                            BigDecimal kcalMeal = BigDecimal.valueOf(meal.getDouble("calories"));
 
-                        // Accumulate totals
-                        totalCarb += carbMeal;
-                        totalProtein += proteinMeal;
-                        totalFat += fatMeal;
-                        totalkcal = totalkcal.add(kcalMeal);
+                            // Accumulate totals
+                            totalCarb += carbMeal;
+                            totalProtein += proteinMeal;
+                            totalFat += fatMeal;
+                            totalkcal = totalkcal.add(kcalMeal);
 
-                        // Inflate and populate meal item view
-                        View mealView = getLayoutInflater()
-                            .inflate(R.layout.diet_items, mealContainer, false);
-                        ((TextView) mealView.findViewById(R.id.mealTimeText)).setText(mealtime);
-                        ((TextView) mealView.findViewById(R.id.nameText)).setText(name);
-                        ((TextView) mealView.findViewById(R.id.carbText))
-                            .setText(String.valueOf(carbMeal));
-                        ((TextView) mealView.findViewById(R.id.proteinText))
-                            .setText(String.valueOf(proteinMeal));
-                        ((TextView) mealView.findViewById(R.id.fatText))
-                            .setText(String.valueOf(fatMeal));
-                        mealContainer.addView(mealView);
+                            // Inflate and populate meal item view
+                            View mealView = getLayoutInflater()
+                                    .inflate(R.layout.diet_items, mealContainer, false);
+                            ((TextView) mealView.findViewById(R.id.mealTimeText)).setText(mealtime);
+                            ((TextView) mealView.findViewById(R.id.nameText)).setText(name);
+                            ((TextView) mealView.findViewById(R.id.carbText))
+                                    .setText(String.valueOf(carbMeal));
+                            ((TextView) mealView.findViewById(R.id.proteinText))
+                                    .setText(String.valueOf(proteinMeal));
+                            ((TextView) mealView.findViewById(R.id.fatText))
+                                    .setText(String.valueOf(fatMeal));
+                            mealContainer.addView(mealView);
+                        }
+
+
+                        // Build chart data from totals
+                        data.add(new ValueDataEntry("탄수화물", totalCarb));
+                        data.add(new ValueDataEntry("단백질", totalProtein));
+                        data.add(new ValueDataEntry("지방", totalFat));
+
+                        Pie pie = AnyChart.pie();
+                        pie.data(data);
+                        pie.title("영양소 비율");
+                        AnyChartView anyChartView = findViewById(R.id.meal_chart_view);
+                        anyChartView.setChart(pie);
+                        // Set total values to summary TextViews
+                        ((TextView) findViewById(R.id.valueCarb)).setText(String.valueOf((int) totalCarb));
+                        ((TextView) findViewById(R.id.valueProtein)).setText(String.valueOf((int) totalProtein));
+                        ((TextView) findViewById(R.id.valueFat)).setText(String.valueOf((int) totalFat));
+                        ((TextView) findViewById(R.id.valueCal)).setText(totalkcal.toPlainString());
+
+
+                    } catch (JSONException e) {
+                        Log.e("DietFetchError", e.toString());
                     }
-
-
-                    // Build chart data from totals
-                    data.add(new ValueDataEntry("탄수화물", totalCarb));
-                    data.add(new ValueDataEntry("단백질", totalProtein));
-                    data.add(new ValueDataEntry("지방", totalFat));
-
-                    Pie pie = AnyChart.pie();
-                    pie.data(data);
-                    pie.title("영양소 비율");
-                    AnyChartView anyChartView = findViewById(R.id.meal_chart_view);
-                    anyChartView.setChart(pie);
-                    // Set total values to summary TextViews
-                    ((TextView) findViewById(R.id.valueCarb)).setText(String.valueOf((int) totalCarb));
-                    ((TextView) findViewById(R.id.valueProtein)).setText(String.valueOf((int) totalProtein));
-                    ((TextView) findViewById(R.id.valueFat)).setText(String.valueOf((int) totalFat));
-                    ((TextView) findViewById(R.id.valueCal)).setText(totalkcal.toPlainString());
-
-
-                } catch (JSONException e) {
-                    Log.e("DietFetchError", e.toString());
-                }
-            },
-            error -> Log.e("DietFetchError", error.toString())
+                },
+                error -> Log.e("DietFetchError", error.toString())
         );
-
         RequestQueue queue = Volley.newRequestQueue(this);
         queue.add(request);
-
-
     }
 
     @Override
@@ -143,12 +141,12 @@ public class DietActivity extends AppCompatActivity {
         // + 버튼 클릭 시 DietAddActivity로 이동
         findViewById(R.id.buttonAddMeal).setOnClickListener(v -> {
             Intent intent = new Intent(DietActivity.this, DietAddActivity.class);
-            intent.putExtra("userId", this.userId);
             startActivityForResult(intent, 100);
         });
 
         fetchDietData(today);
     }
+
     // DietAddActivity에서 돌아왔을 때 결과 처리
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -163,7 +161,15 @@ public class DietActivity extends AppCompatActivity {
         }
         if (requestCode == 100 && resultCode == RESULT_OK) {
             String today = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+
             fetchDietData(today);
+
         }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String today = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+        new Handler().postDelayed(() -> fetchDietData(today), 500);
     }
 }
