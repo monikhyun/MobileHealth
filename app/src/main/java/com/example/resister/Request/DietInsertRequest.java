ccbb7cd3e3208547961fd81d2a6c6946b8b29357
kimjintae <verbal0719@naver.com>
2025-06-08 04:03:23 +0900

    feat: feat/diet 브랜치에서 diet 관련 파일 수동 병합

---
+++ app/src/main/java/com/example/resister/Request/DietInsertRequest.java
@@ -0,0 +1,36 @@
+package com.example.resister.Request;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONObject;
+
+import java.nio.charset.StandardCharsets;
+
+public class DietInsertRequest extends StringRequest {
+    private final JSONObject jsonBody;
+
+    public DietInsertRequest(String userId,
+                             JSONObject jsonBody,
+                             Response.Listener<String> listener,
+                             Response.ErrorListener errorListener) {
+        super(Method.POST,
+                "http://10.0.2.2:8080/api/diet/record/" + userId,
+                listener,
+                errorListener);
+        this.jsonBody = jsonBody;
+    }
+
+    @Override
+    public String getBodyContentType() {
+        return "application/json; charset=utf-8";
+    }
+
+    @Override
+    public byte[] getBody() throws AuthFailureError {
+        return jsonBody == null
+                ? null
+                : jsonBody.toString().getBytes(StandardCharsets.UTF_8);
+    }
+}
